45e548bbfff2ea1d14818287cf17028702119050
paul <paharry@gmail.com>
2012-08-22 14:17:34 +0100

    initial test

---
+++ HelloWorld/src/testHelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author SeanLaptop
+ *
+ */
+public class testHelloWorld
+{
+
+	/**
+	 * 
+	 */
+	public testHelloWorld()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
